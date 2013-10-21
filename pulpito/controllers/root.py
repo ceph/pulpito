0a3587ce779a9bed63546cb5145a2064e52ad3d4
Alfredo Deza <alfredo@deza.pe>
2013-10-21 16:27:08 -0400

    single run controller working

--- pulpito/controllers/root.py
+++ pulpito/controllers/root.py
@@ -39,6 +39,20 @@ class RunController(object):
     @expose('run.html')
     def index(self):
         metadata = requests.get('http://sentry.front.sepia.ceph.com:8080/runs/%s' % self.name).json()
+        for job in metadata['jobs']:
+            job['status_class'] = self.set_status_class(job)
         return dict(
             run=metadata
         )
+
+    def set_status_class(self, job):
+        success = job['success']
+        if success is False:
+            status_class = 'danger'
+        elif success:
+            status_class = 'success'
+        elif success is None:
+            status_class = 'warning'
+        else:
+            status_class = 'warning'
+        return status_class
