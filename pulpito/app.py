f7c05a371f71c4bbed3ca0bf4d693e3546c459b2
Zack Cerza <zack@cerza.org>
2014-01-03 12:55:31 -0600

    Use Dozer Profiler.

--- pulpito/app.py
+++ pulpito/app.py
@@ -1,3 +1,4 @@
+from dozer import Profiler
 from pecan import conf, make_app
 from pulpito import model
 
@@ -7,9 +8,10 @@ def setup_app(config):
     model.init_model()
     app_conf = dict(config.app)
 
-    return make_app(
+    app = make_app(
         app_conf.pop('root'),
         logging=getattr(config, 'logging', {}),
         extra_template_vars=dict(paddles_address=conf.paddles_address),
         **app_conf
     )
+    return Profiler(app, profile_path='/tmp/profiles')
