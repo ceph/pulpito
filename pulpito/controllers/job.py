a80ba7c8a11fde570ef29095ed0e978cb4ecb50c
Zack Cerza <zack@cerza.org>
2014-03-18 16:20:15 -0500

    Add error handling to JobController

--- pulpito/controllers/job.py
+++ pulpito/controllers/job.py
@@ -2,6 +2,7 @@ from pecan import expose
 from pecan import conf
 import requests
 from util import prettify_job
+from pulpito.controllers import error
 
 base_url = conf.paddles_address
 
@@ -10,9 +11,16 @@ class JobController(object):
     def __init__(self, run_name, job_id):
         self.run_name = run_name
         self.job_id = job_id
-        self.job = requests.get("{base}/runs/{run}/jobs/{job}".format(
-            base=base_url, run=run_name, job=job_id)).json()
-        prettify_job(self.job)
+        resp = requests.get("{base}/runs/{run}/jobs/{job}".format(
+            base=base_url, run=run_name, job=job_id))
+
+        if resp.status_code == 400:
+            error('/errors/invalid/')
+        elif resp.status_code == 404:
+            error('/errors/not_found/')
+        else:
+            self.job = resp.json()
+            prettify_job(self.job)
 
     @expose('job.html')
     def index(self):
