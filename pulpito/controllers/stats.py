58cfa05289cfe7a17fa1e933e9e4026549feadd2
Zack Cerza <zack@cerza.org>
2014-04-03 09:25:11 -0500

    Sort node stats by default

--- pulpito/controllers/stats.py
+++ pulpito/controllers/stats.py
@@ -1,3 +1,4 @@
+from collections import OrderedDict
 from pecan import conf, expose, redirect
 import requests
 
@@ -14,10 +15,11 @@ class StatsController(object):
         else:
             uri += '/'
 
-        response = requests.get(uri)
-        if response.status_code != 200:
+        response = requests.get(uri, timeout=60)
+        if response.status_code == 502:
             redirect('/errors?status_code={status}&message={msg}'.format(
-                status=response.status_code, msg=response.text))
+                status=200, msg='502 gateway error :('),
+                internal=True)
 
         nodes = response.json()
         statuses = ['pass', 'fail', 'dead', 'unknown', 'running']
@@ -28,6 +30,8 @@ class StatsController(object):
             for status in statuses:
                 if node.get(status) is None:
                     node[status] = 0
+        nodes = OrderedDict(
+            sorted(nodes.items(), key=lambda t: t[1]['total'], reverse=True))
         return dict(
             machine_type=machine_type,
             nodes=nodes,
