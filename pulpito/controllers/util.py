2ba9c6b7098add005e2c1f54af739157e025e6fb
Zack Cerza <zack@cerza.org>
2014-04-09 14:55:51 -0500

    Update to reflect paddles additions
    Namely, Run.started, Run.updated, Job.started

--- pulpito/controllers/util.py
+++ pulpito/controllers/util.py
@@ -31,14 +31,20 @@ def set_run_status_class(run):
 
 
 def set_run_time_info(run):
-    if 'posted' in run:
-        run['posted'] = remove_msecs(run['posted'])
-    if 'running' in run.get('status', ''):
-        posted_local = utc_stamp_to_local(run['posted'])
-        posted = datetime.strptime(posted_local, timestamp_fmt)
-        run['runtime'] = remove_msecs(str(datetime.now() - posted))
-    if 'scheduled' in run:
+    if run.get('scheduled'):
         run['scheduled'] = remove_msecs(run['scheduled'])
+    if run.get('posted'):
+        run['posted'] = remove_msecs(run['posted'])
+    if run.get('updated'):
+        run['updated'] = remove_msecs(run['updated'])
+    if run.get('started'):
+        run['started'] = remove_msecs(run['started'])
+        started = datetime.strptime(run['started'], timestamp_fmt)
+        if run['status'] == 'running':
+            run['runtime'] = remove_msecs(str(datetime.utcnow() - started))
+        else:
+            updated = datetime.strptime(run['updated'], timestamp_fmt)
+            run['runtime'] = remove_msecs(str(updated - started))
 
 
 def prettify_job(job):
@@ -52,17 +58,19 @@ def set_job_status_class(job):
 
 
 def set_job_time_info(job):
-    if 'posted' in job:
+    if job.get('posted'):
         job['posted_pretty'] = remove_msecs(job['posted'])
-        posted = datetime.strptime(job['posted_pretty'], timestamp_fmt)
-    if 'updated' in job:
+    if job.get('started'):
+        job['started_pretty'] = remove_msecs(job['started'])
+    if job.get('updated'):
         job['updated_pretty'] = remove_msecs(job['updated'])
-        updated = datetime.strptime(job['updated_pretty'], timestamp_fmt)
-    if 'posted' in job and 'updated' in job:
+    if job.get('started') and job.get('updated'):
         if job['status'] == 'running':
-            job['runtime'] = remove_msecs(str(datetime.utcnow() - posted))
+            started = datetime.strptime(job['started_pretty'], timestamp_fmt)
+            job['runtime'] = remove_msecs(str(datetime.utcnow() - started))
         else:
-            job['runtime'] = str(updated - posted)
+            updated = datetime.strptime(job['updated_pretty'], timestamp_fmt)
+            job['runtime'] = str(updated - started)
     if job.get('duration'):
         duration_pretty = str(timedelta(seconds=job['duration']))
         job['duration_pretty'] = duration_pretty
