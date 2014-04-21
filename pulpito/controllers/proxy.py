1b21f98b94bca95c67fb8d53f4c6bbb8360ab316
Zack Cerza <zack@cerza.org>
2014-04-21 15:44:36 -0500

    Add ProxyController
    The purpose of this is to allow clients to indirectly request certain
    queries from paddles in order to populate menus etc. in the UI.

---
+++ pulpito/controllers/proxy.py
@@ -0,0 +1,31 @@
+from pecan import conf, expose
+import requests
+
+
+class ProxyController(object):
+    """
+    Allows the client to indirectly query a paddles instance. Useful if paddles
+    lives behind a firewall, but pulpito does not.
+    """
+    base_url = conf.paddles_address
+
+    def __init__(self, query):
+        self.query = query
+
+    @expose('json')
+    def branches(self):
+        resp = requests.get(self.base_url + '/runs/branch/')
+        resp.raise_for_status()
+        return resp.json()
+
+    @expose('json')
+    def machine_types(self):
+        resp = requests.get(self.base_url + '/runs/machine_type/')
+        resp.raise_for_status()
+        return resp.json()
+
+    @expose('json')
+    def suites(self):
+        resp = requests.get(self.base_url + '/runs/suite/')
+        resp.raise_for_status()
+        return resp.json()
--- pulpito/controllers/root.py
+++ pulpito/controllers/root.py
@@ -6,6 +6,7 @@ from util import prettify_run, prettify_job
 from pulpito.controllers import error
 from pulpito.controllers.errors import ErrorsController
 from pulpito.controllers.compare import RunCompareController
+from pulpito.controllers.proxy import ProxyController
 from pulpito.controllers.stats import StatsController
 from pulpito.controllers.util import get_run_filters
 
@@ -93,6 +94,8 @@ class RootController(object):
 
     @expose()
     def _lookup(self, name, *remainder):
+        if name == '_paddles':
+            return ProxyController(query=name), remainder
         return RunController(name), remainder
 
 
